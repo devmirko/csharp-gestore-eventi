d44b7f7a9fa527cb7611c983d787a48f61fda726
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-11-09 15:02:35 +0100

    creazione set get

---
+++ Evento.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Per prima cosa è necessario creare una classe Evento che abbia i seguenti attributi: 
+//●titolo 
+//●data 
+//●capienza massima dell’evento 
+//●numero di posti prenotati 
+
+public class Evento
+{
+    private string _titolo;
+    public string Titolo
+    {
+        get
+        {
+            return _titolo;
+        }
+        set
+        {
+            _titolo = value;
+
+        }
+    }
+    private DateTime _data;
+    public DateTime Data {
+        get {
+            return _data;
+        }
+        set {
+            if( DateTime.Now > value )
+            {
+                //inserire eccezzione
+                _data = value;
+            }
+        }
+    }
+
+    private int _postiMax;
+
+    public int PostiMax { 
+        get {
+            return _postiMax;
+        } 
+        set {
+            _postiMax = value;
+        } 
+    }
+
+    public int PostiPrenotati { get; private set; }
+
+
+    public Evento(string titolo, DateTime data, int postiMax )
+    {
+      
+
+
+
+    }
+
+   
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -3,21 +3,4 @@ using System.Runtime.ConstrainedExecution;
 
 Console.WriteLine("Hello, World!");
 
-//Per prima cosa è necessario creare una classe Evento che abbia i seguenti attributi: 
-//●titolo 
-//●data 
-//●capienza massima dell’evento 
-//●numero di posti prenotati 
 
-public class Evento
-{
-    public string Titolo { get; set; }
-    public DateTime Data { get; set; }
-
-    public int PostiMax { get; set; }
-
-    public int PostiPrenotati { get; set; }
-
-
-
-}
