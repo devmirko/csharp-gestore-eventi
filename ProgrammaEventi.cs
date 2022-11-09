c0465299d99904c1facde0b64f69fa5d241d0f5a
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-11-09 18:35:03 +0100

    update classe programmaevento

---
+++ ProgrammaEventi.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csharp_gestore_eventi.Exceptions;
+
+public class ProgrammaEventi
+{
+    private string _titolo;
+    public string Titolo { 
+        get {
+            return _titolo;
+        } 
+        set {
+            if( value == null | value == "")
+            {
+                throw new GestoreEventiException("inserire il nome dell'evento");
+            }
+
+            _titolo = value;
+        } 
+    }
+
+    List<Evento> eventi;
+    public ProgrammaEventi(string titolo)
+    {
+        Titolo = titolo;
+        eventi = new List<Evento>();
+    }
+
+    public void AggiungiEvento( Evento evento )
+    {
+        eventi.Add(evento);
+
+    }
+
+    public List<Evento> CercaEventi( DateTime data)
+    {
+        List<Evento> eventidata = new List<Evento>();
+        foreach (Evento evento in eventi)
+        {
+            if (evento.Data == data)
+            {
+                eventidata.Add(evento);
+            }
+        }
+        return eventidata;
+    }
+
+    //public static string StampaEventi(List<Evento> eventi)
+    //{
+    //    foreach ( Evento evento in eventi)
+    //    {
+            
+
+    //    }
+
+    //}
+
+
+
+    
+
+
+
+
+
+}
+
+
+
+
+
+
+
